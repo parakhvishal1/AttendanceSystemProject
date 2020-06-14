faf733b1b41b357e8d151916c1b8ec9e3792090a
unknown <parakhvishal1@gmail.com>
2020-06-14 12:18:01 +0530

    Updated

---
+++ AttendanceSystemDemo/Migrations/20200614064030_Initial Migration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AttendanceSystemDemo.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attendance",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Status = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Remarks = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Date = table.Column<string>(type: "DateTime", unicode: false, maxLength: 50, nullable: true),
+                    Role = table.Column<string>(unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attendance", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StudentName = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Grade = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Gender = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Dob = table.Column<DateTime>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teacher",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TeacherName = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Subject = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Gender = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Dob = table.Column<DateTime>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teacher", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Password = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Role = table.Column<string>(unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attendance");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Teacher");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
